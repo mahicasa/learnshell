62deeb8f937cefea60f5b6d2485554a61fd42a98
mahicasa <mahicasa@gmail.com>
2024-02-10 15:03:23 +0100

    add functions.sh

--- 04-functions.sh
+++ 04-functions.sh
@@ -21,3 +21,11 @@ sample_sum() {
 sample_sum 100 200
 
 # Inside main program all special vars (0-n, *, #) are which are passed to script are not accessible inside functions.
+
+sample2() {
+  echo Hello1
+  return
+  echo Hello2
+}
+
+sample2
