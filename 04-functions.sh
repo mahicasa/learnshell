b46d650c4c0e34487fcaf5a857e7a912c297c901
mahicasa <mahicasa@gmail.com>
2024-02-10 15:01:32 +0100

    add functions.sh

--- 04-functions.sh
+++ 04-functions.sh
@@ -6,3 +6,18 @@ sample1() {
 
 # Main Program
 sample1
+
+# Declared variable in main program can be accessed in function  and it is vice versa
+# Declared variable in main program can be overwritten by function and it is vice versa
+
+# Along with this function have special variables , We can pass arguments to a function
+
+# sample1 100 200
+
+sample_sum() {
+  echo "Addition of $1 and $2 is $(($1+$2))"
+}
+
+sample_sum 100 200
+
+# Inside main program all special vars (0-n, *, #) are which are passed to script are not accessible inside functions.
