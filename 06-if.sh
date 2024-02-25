ab244e904ae963d1e2bb6fc617f6974874e8ddd2
mahicasa <mahicasa@gmail.com>
2024-02-25 18:55:27 +0100

    If condition

--- 06-if.sh
+++ 06-if.sh
@@ -21,3 +21,34 @@
 # else
 #   commands
 # fi
+
+# Expressions
+# 1. Strings Checks
+
+# [ string (operator) string ]
+# Operators:
+  #     =, == -> Check if both the strings are same
+  #     != -> Check if both the strings are not same
+  #     -z -> Check if variable is empty
+
+# Ex:   [ a = x ] [ a != x ], [ $x == abc ], [ -z "$var" ]
+
+## Note: If any where in expressions if you see a variable, Always ensure you give double quotes.
+
+# 2. Number Checks
+
+# Operators : -eq, -ne , -gt, -ge, -le, lt
+
+# 3. File Checks
+
+# Operators: [ -a /tmp/abc ]
+
+if [ -z "$1" ]; then
+  echo Input is empty
+  exit 1
+fi
+
+input=$1
+if [ "$input" = devops ]; then
+  echo Welcome to DevOps Training
+fi
