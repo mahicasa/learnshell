d80de9a48c796d1d9f6874343c5f68fd1ee70e0a
mahicasa <mahicasa@gmail.com>
2024-02-25 18:01:15 +0100

    If condition

---
+++ 06-if.sh
@@ -0,0 +1,23 @@
+# Simple if
+
+# if [ expression ]; then
+#   commands
+# fi
+
+# If Else
+
+# if [ expression ]; then
+#   commands
+# else
+#   commands
+# fi
+
+# Else if
+
+#if [ expression1 ]; then
+#   commands1
+#elif [ expression2 ]; then
+#   commands2
+# else
+#   commands
+# fi
